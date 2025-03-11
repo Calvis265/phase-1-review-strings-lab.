8171be65fd7278ad8dde25f693eefeda6c73ae09
calvis onyango <calvisonyango265@gmail.com>
2025-03-11 12:21:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+ const currentUser='calvis'
+
+ const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+
+ const excitedWelcomeMessage= `WELCOME TO FLATBOOK, CALVIS!`;
+
+ const shortGreeting="Welcome, !"
+ 
+
+
+
+
